14ae46313e82caa5cc6d881d986eca810103ed07
Lee Madajczyk <lee@madajczyk.com>
2020-10-10 18:31:59 -0500

    Removed Now page.

--- serviceworker.js
+++ serviceworker.js
@@ -14,8 +14,7 @@ const cacheList = [
 const offlinePages = [
     '/',
     '/about/',
-    '/archive/',
-    '/now/'
+    '/archive/'
 ];
 
 function updateStaticCache() {
@@ -36,13 +35,13 @@ function updateStaticCache() {
 
 function stashInCache(cacheName, request, response) {
     caches.open(cacheName)
-        .then( cache => cache.put(request, response) );
+        .then(cache => cache.put(request, response));
 }
 
 // Limit the number of items in a specified cache.
 function trimCache(cacheName, maxItems) {
     caches.open(cacheName)
-        .then( cache => {
+        .then(cache => {
             cache.keys()
                 .then(keys => {
                     if (keys.length > maxItems) {
@@ -56,7 +55,7 @@ function trimCache(cacheName, maxItems) {
 // Remove caches whose name is no longer valid
 function clearOldCaches() {
     return caches.keys()
-        .then( keys => {
+        .then(keys => {
             return Promise.all(keys
                 .filter(key => !cacheList.includes(key))
                 .map(key => caches.delete(key))
@@ -66,13 +65,13 @@ function clearOldCaches() {
 
 self.addEventListener('install', event => {
     event.waitUntil(updateStaticCache()
-        .then( () => self.skipWaiting() )
+        .then(() => self.skipWaiting())
     );
 });
 
 self.addEventListener('activate', event => {
     event.waitUntil(clearOldCaches()
-        .then( () => self.clients.claim() )
+        .then(() => self.clients.claim())
     );
 });
 
@@ -97,22 +96,22 @@ self.addEventListener('fetch', event => {
 
         event.respondWith(
             fetch(request)
-                .then( response => {
-                    // NETWORK
-                    // Stash a copy of this page in the pages cache
-                    let copy = response.clone();
-                    if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
-                        stashInCache(staticCacheName, request, copy);
-                    } else {
-                        stashInCache(pagesCacheName, request, copy);
-                    }
-                    return response;
-                })
-                .catch( () => {
-                    // CACHE or FALLBACK
-                    return caches.match(request)
-                        .then( response => response || caches.match('/offline') );
-                })
+            .then(response => {
+                // NETWORK
+                // Stash a copy of this page in the pages cache
+                let copy = response.clone();
+                if (offlinePages.includes(url.pathname) || offlinePages.includes(url.pathname + '/')) {
+                    stashInCache(staticCacheName, request, copy);
+                } else {
+                    stashInCache(pagesCacheName, request, copy);
+                }
+                return response;
+            })
+            .catch(() => {
+                // CACHE or FALLBACK
+                return caches.match(request)
+                    .then(response => response || caches.match('/offline'));
+            })
         );
         return;
     }
@@ -120,25 +119,25 @@ self.addEventListener('fetch', event => {
     // For non-HTML requests, look in the cache first, fall back to the network
     event.respondWith(
         caches.match(request)
-            .then(response => {
-                // CACHE
-                return response || fetch(request)
-                    .then( response => {
-                        // NETWORK
-                        // If the request is for an image, stash a copy of this image in the images cache
-                        if (request.headers.get('Accept').includes('image')) {
-                            let copy = response.clone();
-                            stashInCache(imagesCacheName, request, copy);
-                        }
-                        return response;
-                    })
-                    .catch( () => {
-                        // OFFLINE
-                        // If the request is for an image, show an offline placeholder
-                        if (request.headers.get('Accept').includes('image')) {
-                            return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store'}});
-                        }
-                    });
-            })
+        .then(response => {
+            // CACHE
+            return response || fetch(request)
+                .then(response => {
+                    // NETWORK
+                    // If the request is for an image, stash a copy of this image in the images cache
+                    if (request.headers.get('Accept').includes('image')) {
+                        let copy = response.clone();
+                        stashInCache(imagesCacheName, request, copy);
+                    }
+                    return response;
+                })
+                .catch(() => {
+                    // OFFLINE
+                    // If the request is for an image, show an offline placeholder
+                    if (request.headers.get('Accept').includes('image')) {
+                        return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', { headers: { 'Content-Type': 'image/svg+xml', 'Cache-Control': 'no-store' } });
+                    }
+                });
+        })
     );
 });
